80e508c6230bab6cab797b9c9ac6ad7284b0b921
Miriam Espana Acebal <miriam.espana@canonical.com>
2022-06-21 15:50:19 +0200

    Initial commit for v6.0.106

--- debian/build-dotnet-tarball.sh
+++ debian/build-dotnet-tarball.sh
@@ -42,8 +42,8 @@ function on_exit {
     folders_to_clean=("foo" "${tarballname}" "${tempdir}" \
                       "fixup-previously-source-built-artifacts" ".dotnet")
 
-    for folder in ${folders_to_clean[@]}; do
-        if [ -d $folder ]; then
+    for folder in "${folders_to_clean[@]}"; do
+        if [ -d "$folder" ]; then
             rm -rf "$folder"
         fi
     done
@@ -58,8 +58,8 @@ function clean_dotnet_cache {
                     "/tmp/dotnet.*" "/tmp/clr-debug-pipe*" "/tmp/Razor-Server" "/tmp/CoreFxPipe*" "/tmp/VBCSCompiler" \
                     "/tmp/.NETFramework*")
 
-    for folder in ${folders_cached[@]}; do
-        if [ -d $folder ]; then
+    for folder in "${folders_cached[@]}"; do
+        if [ -d "$folder" ]; then
             rm -rf "$folder"
         fi
     done
@@ -67,8 +67,7 @@ function clean_dotnet_cache {
 }
 
 function clean_uscan_download {
-   find .. -name "dotnet*${tag}*${tarball_suffix}" -delete
-   find .. -name "dotnet*${tag}*.tar.xz" -delete
+   find .. -name "dotnet*${tag}*.tar.*" -delete
 }
 
 function check_bootstrap_environment {
@@ -90,7 +89,8 @@ function runtime_id {
 
     source /etc/os-release
 
-    echo "${ID}.${VERSION_ID}-${arch}"
+    #echo "${ID}.${VERSION_ID}-${arch}"
+    [ -n "${VERSION_ID}" ] && echo "${ID}.${VERSION_ID}-${arch}" || echo "${ID}-${arch}"
 }
 
 build_bootstrap=false
@@ -190,7 +190,7 @@ if [[ ${build_bootstrap} == true ]]; then
     fi
 
     pushd packages/archive/
-    curl -O $(cat archiveArtifacts.txt)
+    curl -O "$(cat archiveArtifacts.txt)"
     popd
 
     mkdir foo
@@ -222,22 +222,26 @@ fi
 # not-very-useful artifacts to reduce tarball size
 
 # Binaries for gradle
-rm -r src/aspnetcore.*/src/SignalR/clients/java/signalr/gradle*
+rm -r src/aspnetcore/src/SignalR/clients/java/signalr/gradle*
 
 # Unnecessary crypto implementation: IDEA
-rm -r src/runtime.*/src/tests/JIT/Performance/CodeQuality/Bytemark/
+rm -r src/runtime/src/tests/JIT/Performance/CodeQuality/Bytemark/
 
 # https://github.com/dotnet/aspnetcore/issues/34785
-find src/aspnetcore.*/src -type d -name samples -print0 | xargs -0 rm -r
+find src/aspnetcore/src -type d -name samples -print0 | xargs -0 rm -r
 
 # https://github.com/NuGet/Home/issues/11094
-rm -r src/nuget-client.*/test/EndToEnd
+rm -r src/nuget-client/test/EndToEnd
 
 # https://github.com/Humanizr/sample-aspnetmvc/issues/1
-rm -r src/source-build.*/src/humanizer/samples/
+rm -r src/source-build/src/humanizer/samples/
 
 #Non-free and unnecesary help file for 7-zip
-rm src/source-build.*/src/newtonsoft-json901/Tools/7-zip/7-zip.chm
+rm src/source-build/src/newtonsoft-json901/Tools/7-zip/7-zip.chm
+
+#Checked that are not needed in the build: this only removes under roslyn:
+#src/roslyn/src/Compilers/Test/Resources/Core/SymbolsTests/V?/*.dll
+find . -iname "*.dll" -exec rm -rf {} +
 
 popd
 
